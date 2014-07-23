97e78b39f594a2bc14fe10875a03a5ec42d19362
benmckellick <benjamn8@hotmail.com>
2014-07-22 22:17:05 -0400

    new tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,38 @@ class TweetsController < ApplicationController
 				format.html {redirect_to @tweet, notice: 'Tweet successfully created.' }
 			else format.html {render :new}
 			end
+
+	def show
+	end
+	def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tweet }
+      else
+        format.html { render :edit }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  	def destroy
+    @tweet.destroy
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  	private
+    
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    def tweet_params
+      params.require(:tweet).permit(:content, :bird_id)
+    end
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ActiveRecord::Base
 	belongs_to :user
+
+	validates :content, length: {minimum: 4}
 end
